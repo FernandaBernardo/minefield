1f5c9412d6b95be5f63a392e99614b3590e1ade9
Fernanda Bernardo <fernandabernardo94@gmail.com>
2017-03-26 10:11:48 -0300

    count mines

--- src/main/Minefield.java
+++ src/main/Minefield.java
@@ -2,4 +2,43 @@ package main;
 
 public class Minefield {
 
+	private String[][] minefield;
+	private int lines;
+	private int columns;
+	private int mines;
+
+	public Minefield(String minefieldToConstroy, int lines, int columns) {
+		this.lines = lines;
+		this.columns = columns;
+		formatMinefield(minefieldToConstroy);
+	}
+
+	private void formatMinefield(String minefieldToConstroy) {
+		this.minefield = new String[lines][columns];
+		String[] lines = minefieldToConstroy.split("\n");
+		for (int i = 0; i < lines.length; i++) {
+			String[] squares = lines[i].split(" ");
+			for (int j = 0; j < squares.length; j++) {
+				minefield[i][j] = squares[j];
+			}
+		}
+	}
+
+	public void resolve() {
+		for (int i = 0; i < lines; i++) {
+			for (int j = 0; j < columns; j++) {
+				if(isMine(minefield[i][j])) {
+					this.mines++;
+				}
+			}
+		}
+	}
+
+	private boolean isMine(String square) {
+		return square.equals("*");
+	}
+
+	public int getMines() {
+		return mines;
+	}
 }
--- src/test/MinefieldTest.java
+++ src/test/MinefieldTest.java
@@ -4,11 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import main.Minefield;
+
 public class MinefieldTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void shouldCountMines() {
+		Minefield minefield = new Minefield("* . . .\n"
+				+ ". . . .\n"
+				+ ". * . .\n"
+				+ ". . . .\n", 4, 4);
+		minefield.resolve();
+		assertEquals(2, minefield.getMines());
 	}
 
 }
