634041d3360c7260e1a9cce939ce6e0558bad4ce
Fernanda Bernardo <fernandabernardo94@gmail.com>
2017-03-26 10:47:26 -0300

    resolve minefield

--- src/main/Minefield.java
+++ src/main/Minefield.java
@@ -15,30 +15,71 @@ public class Minefield {
 
 	private void formatMinefield(String minefieldToConstroy) {
 		this.minefield = new String[lines][columns];
-		String[] lines = minefieldToConstroy.split("\n");
-		for (int i = 0; i < lines.length; i++) {
-			String[] squares = lines[i].split(" ");
-			for (int j = 0; j < squares.length; j++) {
+		String[] linesString = minefieldToConstroy.split("\n");
+		for (int i = 0; i < lines; i++) {
+			String[] squares = linesString[i].split(" ");
+			for (int j = 0; j < columns; j++) {
 				minefield[i][j] = squares[j];
 			}
 		}
 	}
 
+	private String minefieldToString() {
+		String newMinefield = "";
+		for(int i = 0; i < lines; i++) {
+			for(int j = 0; j < columns; j++) {
+				newMinefield = newMinefield.concat(minefield[i][j] + " ");
+			}
+			newMinefield = newMinefield.trim();
+			newMinefield = newMinefield.concat("\n");
+		}
+		newMinefield = newMinefield.trim();
+		return newMinefield;
+	}
+
 	public void resolve() {
 		for (int i = 0; i < lines; i++) {
 			for (int j = 0; j < columns; j++) {
-				if(isMine(minefield[i][j])) {
+				if(isMine(i, j)) {
 					this.mines++;
+				} else {
+					fillTips(i, j);
 				}
 			}
 		}
 	}
 
-	private boolean isMine(String square) {
-		return square.equals("*");
+	private void fillTips(int line, int column) {
+		int count = 0;
+
+		if(line - 1 >= 0) {
+			if(isMine(line - 1, column)) count++;
+			if(column - 1 >= 0 && isMine(line - 1, column - 1)) count++;
+			if(column + 1 < columns && isMine(line - 1, column + 1)) count++;
+		}
+
+		if(column - 1 >= 0 && isMine(line, column - 1)) count++;
+		if(column + 1 < columns && isMine(line, column + 1)) count++;
+
+		if(line + 1 < lines) {
+			if(isMine(line + 1, column)) count++;
+			if(column - 1 >= 0 && isMine(line + 1, column - 1)) count++;
+			if(column + 1 < columns && isMine(line + 1, column + 1)) count++;
+		}
+
+		minefield[line][column] = String.valueOf(count);
+	}
+
+	private boolean isMine(int line, int column) {
+		return minefield[line][column].equals("*");
 	}
 
 	public int getMines() {
 		return mines;
 	}
+
+	public String getMinefield() {
+		return minefieldToString();
+	}
+
 }
--- src/test/MinefieldTest.java
+++ src/test/MinefieldTest.java
@@ -18,4 +18,17 @@ public class MinefieldTest {
 		assertEquals(2, minefield.getMines());
 	}
 
+	@Test
+	public void shouldResolveMinefieldWithCenterMines() {
+		Minefield minefield = new Minefield("* . . .\n"
+				+ ". . . .\n"
+				+ ". * . .\n"
+				+ ". . . .\n", 4, 4);
+		minefield.resolve();
+		String expected = "* 1 0 0\n"
+				+ "2 2 1 0\n"
+				+ "1 * 1 0\n"
+				+ "1 1 1 0";
+		assertEquals(expected, minefield.getMinefield());
+	}
 }
